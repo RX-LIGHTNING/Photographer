64463363ad1abc8a91e4f9b0171a3ea138901cae
Roman Kuznetsov <mmamano@inbox.ru>
2022-10-19 10:04:49 +0300

    Added post mapping for adding a record

--- src/main/java/com/photo/photographer/controller/PhotoController.java
+++ src/main/java/com/photo/photographer/controller/PhotoController.java
@@ -3,15 +3,21 @@ package com.photo.photographer.controller;
 import com.photo.photographer.dto.Photo;
 import com.photo.photographer.repo.PhotoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class PhotoController {
     @Autowired
     PhotoRepo photoRepo;
-    @GetMapping("/photo")
-    Iterable<Photo> getPhoto(){
+    @GetMapping("/photos")
+    Iterable<Photo> getPhotos(){
+        System.out.println(2);
         return photoRepo.findAll();
+    }
+    @PostMapping("/photo/add")
+    Photo putPhoto(@RequestBody Photo photo){
+        return photoRepo.save(photo);
     }
 }
--- src/main/java/com/photo/photographer/repo/PhotoRepo.java
+++ src/main/java/com/photo/photographer/repo/PhotoRepo.java
@@ -4,5 +4,4 @@ import com.photo.photographer.dto.Photo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PhotoRepo extends CrudRepository<Photo, Long> {
-
 }
